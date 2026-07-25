1dc3057daa779cd8a7f1effdbbcd6bc445199d09
Nguyen-huu-trinh <Nguyenhuutrinh251206@gmail.com>
2026-07-25 23:25:47 +0700

    fix api

--- app/api/chapters/[id]/route.ts
+++ app/api/chapters/[id]/route.ts
@@ -1,55 +1,55 @@
-import { NextRequest, NextResponse } from "next/server";
-import { chapterService } from "@/services/chapter.service";
-import { UpdateChapterSchema } from "@/validators/chapter.schema";
+// import { NextRequest, NextResponse } from "next/server";
+// import { chapterService } from "@/services/chapter.service";
+// import { UpdateChapterSchema } from "@/validators/chapter.schema";
 
-interface Props {
-  params: Promise<{
-    id: string;
-  }>;
-}
+// interface Props {
+//   params: Promise<{
+//     id: string;
+//   }>;
+// }
 
-export async function GET(
-  request: NextRequest,
-  { params }: Props
-) {
-  const { id } = await params;
+// export async function GET(
+//   request: NextRequest,
+//   { params }: Props
+// ) {
+//   const { id } = await params;
 
-  return NextResponse.json(
-    await chapterService.getById(id)
-  );
-}
+//   return NextResponse.json(
+//     await chapterService.getById(id)
+//   );
+// }
 
-export async function PATCH(
-  request: NextRequest,
-  { params }: Props
-) {
-  const body =
-    await request.json();
+// export async function PATCH(
+//   request: NextRequest,
+//   { params }: Props
+// ) {
+//   const body =
+//     await request.json();
 
-  const values =
-    UpdateChapterSchema.parse(
-      body
-    );
+//   const values =
+//     UpdateChapterSchema.parse(
+//       body
+//     );
 
-  const { id } = await params;
+//   const { id } = await params;
 
-  return NextResponse.json(
-    await chapterService.update(
-      id,
-      values
-    )
-  );
-}
+//   return NextResponse.json(
+//     await chapterService.update(
+//       id,
+//       values
+//     )
+//   );
+// }
 
-export async function DELETE(
-  request: NextRequest,
-  { params }: Props
-) {
-  const { id } = await params;
+// export async function DELETE(
+//   request: NextRequest,
+//   { params }: Props
+// ) {
+//   const { id } = await params;
 
-  await chapterService.delete(id);
+//   await chapterService.delete(id);
 
-  return NextResponse.json({
-    success: true,
-  });
-}
+//   return NextResponse.json({
+//     success: true,
+//   });
+// }

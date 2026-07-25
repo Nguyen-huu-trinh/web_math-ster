2f8a35d0563faa0d658921ebc4325862d139d896
Nguyen-huu-trinh <Nguyenhuutrinh251206@gmail.com>
2026-07-25 23:40:09 +0700

    update5

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,23 +1,23 @@
-import { NextResponse } from "next/server";
-import { userService } from "@/services/user.service";
+// import { NextResponse } from "next/server";
+// import { userService } from "@/services/user.service";
 
-export async function GET() {
-  const students =
-    await userService.getStudents();
+// export async function GET() {
+//   const students =
+//     await userService.getStudents();
 
-  const teachers =
-    await userService.getTeachers();
+//   const teachers =
+//     await userService.getTeachers();
 
-  return NextResponse.json({
-    students,
-    teachers,
-  });
-}
+//   return NextResponse.json({
+//     students,
+//     teachers,
+//   });
+// }
 
-export async function POST(request: Request) {
-  const body = await request.json();
+// export async function POST(request: Request) {
+//   const body = await request.json();
 
-  const user = await userService.create(body);
+//   const user = await userService.create(body);
 
-  return NextResponse.json(user);
-}
+//   return NextResponse.json(user);
+// }

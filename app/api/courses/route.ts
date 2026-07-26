7ea680bd8bf6bd490b89c07252ff33d10cec2460
Nguyen-huu-trinh <Nguyenhuutrinh251206@gmail.com>
2026-07-27 02:15:23 +0700

    update course

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { courseService } from "@/services/course.service";
 
 export async function GET() {
@@ -6,6 +6,29 @@ export async function GET() {
     const data = await courseService.getAll();
 
     return NextResponse.json(data);
+  } catch (err) {
+    console.error(err);
+
+    return NextResponse.json(
+      {
+        error: String(err),
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const course = await courseService.create(body);
+
+    return NextResponse.json(course, {
+      status: 201,
+    });
   } catch (err) {
     console.error(err);
 

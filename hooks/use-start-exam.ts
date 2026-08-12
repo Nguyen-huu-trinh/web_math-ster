74decbb3134929df855ebb43e54b1af7a8737bc6
Nguyen-huu-trinh <Nguyenhuutrinh251206@gmail.com>
2026-08-12 19:36:40 +0700

    update

--- hooks/use-start-exam.ts
+++ hooks/use-start-exam.ts
@@ -1,19 +1,12 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 
 import { studentExamClientService } from "@/services/student-exam-client.service";
-import { queryKeys } from "@/lib/react-query/query-keys";
 
 export function useStartExam() {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationFn: (examId: string) =>
-      studentExamClientService.startExam(
-        examId
-      ),
-    onSuccess: () =>
-      queryClient.invalidateQueries({
-        queryKey: queryKeys.student.myExams(),
-      }),
-  });
+    return useMutation({
+        mutationFn: (examId: string) =>
+            studentExamClientService.startExam(
+                examId
+            ),
+    });
 }
